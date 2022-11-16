dc15cfe8ced197cbb1985733057282555240c6df
Allan Jardine <allan.jardine@sprymedia.co.uk>
2022-11-16 17:12:44 +0000

    b343af8c59e921a068a47d3ce933db0768114fee
    Sync to source repo @343af8c59e921a068a47d3ce933db0768114fee

--- js/scroller.semanticui.min.mjs
+++ js/scroller.semanticui.min.mjs
@@ -1,4 +1,4 @@
 /*! Semanic UI styling wrapper for Scroller
  * © SpryMedia Ltd - datatables.net/license
  */
-import $ from"jquery";import DataTable from"datatables.net-se";import DataTable from"datatables.net-scroller";export default DataTable;
+import $ from"jquery";import DataTable from"datatables.net-se";import Scroller from"datatables.net-scroller";export default DataTable;
--- js/scroller.semanticui.mjs
+++ js/scroller.semanticui.mjs
@@ -1,11 +1,10 @@
-
 /*! Semanic UI styling wrapper for Scroller
  * © SpryMedia Ltd - datatables.net/license
  */
 
 import $ from 'jquery';
 import DataTable from 'datatables.net-se';
-import DataTable from 'datatables.net-scroller';
+import Scroller from 'datatables.net-scroller';
 
 
 
